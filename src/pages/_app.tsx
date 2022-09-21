1c389ef8e98f0fb5c861184a7666706fb19d6c43
Raul Val <raulval@outlook.com>
2022-09-21 14:24:08 -0300

    feat: applies path mapping

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "../styles/globals";
-import Theme from "../styles/theme";
+import { GlobalStyle } from "styles/globals";
+import Theme from "styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={Theme}>
-      <Component {...pageProps} />
       <GlobalStyle />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
