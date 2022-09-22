435560f7d838d5331f4d522130ca1cb76ef0fd2c
Raul Val <raulval@outlook.com>
2022-09-22 18:18:08 -0300

    feat: fecthes data to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,88 @@
-import Header from "components/Header";
-import type { NextPage } from "next";
-import { HomeContainer } from "styles/home";
+import { Card, Header, ScrollToTop } from "components";
+import fsPromises from "fs/promises";
+import { GetStaticProps } from "next";
+import { useRouter } from "next/router";
+import path from "path";
+import { useState } from "react";
+// import { cars as data } from "services/database.json";
+import { Car, CarData } from "shared/interfaces";
+import { CardsContainer, HomeContainer } from "styles/home";
+import Theme from "styles/theme";
 
-const Home: NextPage = () => {
-  const handleClickSearch = () => {};
+const Home = ({ cars }: CarData) => {
+  const router = useRouter();
+  const [carsList, setCarsList] = useState<Car[]>(cars);
+
+  console.log(cars);
+
+  const onClickCard = (car: Car) => {};
+
+  const handleClickSearch = (
+    e: any,
+    city: string,
+    startDate: string,
+    finalDate: string
+  ) => {
+    e.preventDefault();
+    startDate = new Date(startDate).toLocaleDateString("pt-BR", {
+      timeZone: "UTC",
+    });
+
+    if (city && startDate && finalDate) {
+      const filteredCars = carsList.filter((car) => {
+        return car.city === city && startDate >= car.availableAt;
+      });
+
+      setCarsList(filteredCars);
+    }
+  };
 
   return (
     <HomeContainer>
       <Header onClickSearch={handleClickSearch} />
-      <h1>Home Page</h1>
+      <ScrollToTop />
+      <CardsContainer>
+        {carsList.length > 0 ? (
+          carsList.map((car: Car) => {
+            return (
+              <Card key={car.id} onClick={() => onClickCard(car)} car={car} />
+            );
+          })
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <p>No cars avaliable</p>
+            <br />
+            <a
+              onClick={() => window.location.reload()}
+              style={{
+                cursor: "pointer",
+                color: `${Theme.colors.primary.main}`,
+              }}
+            >
+              Refresh page
+            </a>
+          </div>
+        )}
+      </CardsContainer>
     </HomeContainer>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const filePath = path.join(process.cwd(), "src/services/database.json");
+  const jsonData = await fsPromises.readFile(filePath);
+  const carsData = JSON.parse(jsonData.toString());
+
+  return {
+    props: carsData,
+    revalidate: 60 * 60 * 1, // 1 hour
+  };
 };
 
 export default Home;
