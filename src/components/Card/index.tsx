af10d4753742b652d7dfe6e27cd819c307b03c08
Raul Val <raulval@outlook.com>
2022-09-22 18:10:12 -0300

    feat: creates card component for cars

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,49 @@
+import {
+  CardBrand,
+  CardBrandModelContainer,
+  CardHeader,
+  CardIcon,
+  CardImage,
+  CardImageContainer,
+  CardModel,
+  CardPrice,
+  CardPriceContainer,
+  CardPriceCurrency,
+  CardPricePeriod,
+  CardPriceWrapper,
+  CardWrapper,
+} from "./styles";
+
+import { Car } from "shared/interfaces";
+
+interface CardProps {
+  car: Car;
+  onClick: () => void;
+}
+
+const Card = ({ car, onClick }: CardProps) => {
+  return (
+    <CardWrapper onClick={onClick}>
+      <CardHeader>
+        <CardBrandModelContainer>
+          <CardBrand>{car.brand}</CardBrand>
+          <CardModel>{car.model}</CardModel>
+        </CardBrandModelContainer>
+        <CardIcon />
+      </CardHeader>
+      <CardImageContainer>
+        <CardImage src={car.homeImage} />
+      </CardImageContainer>
+      <CardPriceContainer>
+        <p className="book-now">Book Now</p>
+        <CardPriceWrapper>
+          <CardPriceCurrency>$</CardPriceCurrency>
+          <CardPrice>{car.price}</CardPrice>
+          <CardPricePeriod>/day</CardPricePeriod>
+        </CardPriceWrapper>
+      </CardPriceContainer>
+    </CardWrapper>
+  );
+};
+
+export default Card;
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,100 @@
+import { BsThreeDots } from "react-icons/bs";
+import styled from "styled-components";
+
+export const CardWrapper = styled.div`
+  width: 287px;
+  height: 223px;
+
+  background-color: ${(props) => props.theme.colors.card.background};
+  border-radius: 20px;
+  cursor: pointer;
+  transition: all 0.2s;
+
+  &:hover {
+    background: transparent linear-gradient(52deg, #a1a7f4 0%, #e6d3f1 100%) 0%
+      0% no-repeat padding-box;
+    scale: 1.1;
+
+    .book-now {
+      opacity: 1;
+      color: white;
+      font: normal normal 600 17px ${(props) => props.theme.font};
+    }
+  }
+`;
+
+export const CardHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const CardBrandModelContainer = styled.div`
+  padding: 23px 0 0 17px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardBrand = styled.p`
+  font: normal normal bold 17px ${(props) => props.theme.font};
+  color: ${(props) => props.theme.colors.primary.text};
+`;
+
+export const CardModel = styled.p`
+  font: normal normal 300 17px ${(props) => props.theme.font};
+  color: ${(props) => props.theme.colors.primary.text};
+  text-transform: uppercase;
+`;
+
+export const CardIcon = styled(BsThreeDots)`
+  width: 30px;
+  height: 30px;
+  margin: 20px 16px 0 0;
+  color: ${(props) => props.theme.colors.primary.text};
+  opacity: 0.25;
+`;
+
+export const CardImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 18px 0 15px 0;
+`;
+
+export const CardImage = styled.img`
+  width: 85%;
+  object-fit: cover;
+`;
+
+export const CardPriceContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-right: 16px;
+
+  .book-now {
+    opacity: 0;
+    margin: 8px 0 0 16px;
+  }
+`;
+
+export const CardPriceWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const CardPriceCurrency = styled.p`
+  font: normal normal bold 14px ${(props) => props.theme.font};
+  color: ${(props) => props.theme.colors.primary.text};
+  text-align: top;
+`;
+
+export const CardPrice = styled.p`
+  font: normal normal bold 24px ${(props) => props.theme.font};
+  color: ${(props) => props.theme.colors.primary.text};
+`;
+
+export const CardPricePeriod = styled.p`
+  padding-top: 14px;
+  font: normal normal 400 14px ${(props) => props.theme.font};
+  color: ${(props) => props.theme.colors.primary.text};
+`;
